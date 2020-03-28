93a387cc3ecdf4d1600cdb7f8de5c21be26d9706
Marcin Skrok <253434@student.pwr.edu.pl>
2020-03-28 13:08:46 +0100

    28.03.2020

--- Mineswepper.cpp
+++ Mineswepper.cpp
@@ -1,136 +1,18 @@
-﻿#include <iostream>
+#include <iostream>
 #include "MineswepperBoard.h"
 #include <stdlib.h>
-
-
-void test(MinesweeperBoard &game)
-{ 
-   /* game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;    
-    std::cin.get();
-    system("cls");
-
-    game.revealField(2, 4);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;   
-    std::cin.get();
-    system("cls");
-
-    game.toggleFlag(2, 1);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;   
-    std::cin.get();
-    system("cls");
-
-       
-    game.revealField(2, 1);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;   
-    std::cin.get();
-    system("cls");
-
-    
-    game.revealField(0, 0);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;    
-    std::cin.get();
-    system("cls");
-
-    
-    game.toggleFlag(2, 1);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;   
-    std::cin.get();
-    system("cls");
-
-    
-    game.revealField(2, 1);
-    game.debug_display();
-    std::cout << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;   
-    std::cin.get();
-    system("cls");    
-    std::cin.get(); */   
-
-    game.debug_display();
-    std::cout <<std::endl<< "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;
-    std::cin.get();
-    system("cls");
-    
-    game.revealField(0, 0);
-    game.toggleFlag(2, 1);
-    game.debug_display();
-    std::cout << std::endl << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;
-    std::cin.get();
-    system("cls");
-    
-    game.revealField(2, 1);
-    game.debug_display();
-    std::cout << std::endl << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;
-    std::cin.get();
-    system("cls");
-
-
-    game.toggleFlag(2, 1);
-    game.debug_display();
-    std::cout << std::endl << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;
-    std::cin.get();
-    system("cls");
-
-    game.revealField(2, 1);
-    game.debug_display();
-    std::cout << std::endl << "Czy pole (2,1) jest odkryte? " << game.isRevealed(2, 1) << std::endl;
-    std::cout << "Czy pole (2,1) ma falge? " << game.hasFlag(2, 1) << std::endl;
-    std::cout << "Ile min wokol pola (2,1)? " << game.countMines(2, 1) << std::endl;
-    std::cout << "Info o polu (2,1): " << game.getFieldInfo(2, 1) << std::endl;
-    std::cin.get(); 
-
-   /* game.debug_display();
-    std::cout << "Ile min wokol pola? " << game.countMines(9,1) << std::endl;
-    std::cout << game.getFieldInfo(1, 0);
-    std::cin.get();*/
-}
-
-    
-
+#include "MSBoardTextView.h"
 
 
 int main() 
 {
-    MinesweeperBoard game(10, 10, DEBUG); //szerokosc, wysokosc
-    test(game);
+    MinesweeperBoard board(10, 10, EASY); //szerokosc, wysokosc
+    MSBoardTextView view (board);
+    view.display();
+    std::cin.get();
+    board.revealField(0, 5);
+    view.display();
+    std::cin.get();
+
     return 0;
 }
