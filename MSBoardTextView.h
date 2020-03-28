946012c4bf74461c302ee7a930d07e64c8936f7b
Marcin Skrok <253434@student.pwr.edu.pl>
2020-03-28 13:07:24 +0100

    28.03.2020

---
+++ MSBoardTextView.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "MineswepperBoard.h"
+
+class MSBoardTextView
+{
+	MinesweeperBoard & board;
+	int width;
+	int height;
+public:
+	explicit MSBoardTextView(MinesweeperBoard & board);
+	void display() const;
+};
