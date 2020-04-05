add1d249d814cfab46256d91f9842679c63587c9
Marcin Skrok <253434@student.pwr.edu.pl>
2020-04-05 12:48:47 +0200

    Add new file

---
+++ MSTextController.cpp
@@ -0,0 +1,52 @@
+#include "MSTextController.h"
+#include "MineswepperBoard.h"
+#include "MSBoardTextView.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+MSTextController::MSTextController(MinesweeperBoard& board, MSBoardTextView& view):board(board),view(view)
+{
+}
+
+void MSTextController::play()
+{
+	int x, y, option;
+	
+	while (board.getGameState() == 0)
+	{
+		board.debug_display();
+		cout << "Maksymalna wspolrzedna x wynosi: " << board.getBoardWidth() << endl;
+		cout << "Maksymalna wspolrzedna y wynosi: " << board.getBoardHeight() << endl << endl;
+		view.display();
+		cout << "Pozostalo min: " << board.getMineCount() << endl;
+		
+		cout << "Wybierz pole:"<<std::endl;
+		cout << "Podaj wspolrzedna x: ";
+		cin >> x;
+		cout << "Podaj wspolrzedna y: ";
+		cin >> y;
+		
+		cout << "1. Odslon pole" << endl;
+		cout << "2. Oflaguj pole" << endl;
+		cout << "Co chcesz zrobic?";
+		cin >> option;
+
+		switch (option) {
+		case 1:
+			board.revealField(x-1, y-1);
+			break;
+		case 2:
+			board.toggleFlag(x - 1, y - 1);
+			break;
+		}
+		
+		system("cls");
+	}
+	if (board.getGameState() == 2)
+		cout << "Niestety, tym razem sie nie udalo";
+	else cout << "Gratulacje, wygrales!";
+	cin.get();
+	cin.get();
+}
