d5fd72c7e760db86d12482eeef776ae6bab7aebd
Marcin Skrok <253434@student.pwr.edu.pl>
2020-04-05 12:49:39 +0200

    Add new file

---
+++ MSTextController.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "MineswepperBoard.h"
+#include "MSBoardTextView.h"
+
+class MSTextController
+{
+	MinesweeperBoard & board;
+	MSBoardTextView & view;
+	
+public:
+	MSTextController(MinesweeperBoard& board, MSBoardTextView& view);
+	void play();
+
+};
+
